87034e216c0b870e74faba8acc65516166bdc7e9
Vojtech <fargy33@gmail.com>
2020-01-13 00:11:50 +0100

    cofeetime

--- scripts/main.js
+++ scripts/main.js
@@ -169,6 +169,19 @@ $('.slider').slick({
 
 });
 
+$('input').focus(function(){
+  $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function(){
+  var inputValue = $(this).val();
+  if ( inputValue == "" ) {
+    $(this).removeClass('filled');
+    $(this).parents('.form-group').removeClass('focused');
+  } else {
+    $(this).addClass('filled');
+  }
+})
 
 $('.view-gallery button').on('click', function() {
   $('.box').toggleClass('animated');
